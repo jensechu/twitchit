d6d672456b0400aaa16fa9947a6901b676ca12d9
Jensen <jensechu@gmail.com>
2014-11-29 08:55:21 -0800

    Stylesheet only updates if not current

--- css_updater.rb
+++ css_updater.rb
@@ -1,21 +1,29 @@
 require "redd"
 require "dotenv"
-require "awesome_print"
 
-Dotenv.load
+Dotenv.load(File.join(File.dirname(__FILE__), '.env'))
 
 SUBREDDIT = "girlgamerscss"
-template  = File.join(File.dirname(__FILE__), "template.css")
 
-@css_contents = open(template, &:read)
-@redd         = Redd::Client::Authenticated.new_from_credentials(ENV["USERNAME"], ENV["PASSWORD"])
+template        = File.join(File.dirname(__FILE__), "template.css")
+@active_template = File.join(File.dirname(__FILE__), "current.css")
+
+@new_css    = open(template, &:read)
+@active_css = open(@active_template, &:read)
+
+@redd = Redd::Client::Authenticated.new_from_credentials(ENV["USERNAME"], ENV["PASSWORD"])
 
 def updateStreamers(streamers)
   streamers.each { |user|
     user.sub! "_", "-" #Reddit image paths cannot contain underscores
-    @css_contents.sub! "url(%%#{user}-twitch-offline%%)", "url(%%#{user}-twitch-online%%)"
+    @new_css.sub! "url(%%#{user}-twitch-offline%%)", "url(%%#{user}-twitch-online%%)"
   }
 
-  result = @redd.edit_stylesheet(SUBREDDIT, @css_contents)
-  ap result
+  if @active_css == @new_css
+    puts 'Active stylesheet is current.'
+    exit
+  end
+
+  File.open(@active_template, 'w') { |file| file.write(@new_css) }
+  result = @redd.edit_stylesheet(SUBREDDIT, @new_css)
 end
